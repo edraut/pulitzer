1a2098e8f2bb9a9c2fec7cd7ff4373cabb0c9b59
gomayonqui <escribimepues@gmail.com>
2015-07-03 16:31:12 -0500

    Add a single post_type link helper

--- app/helpers/pulitzer/posts_helper.rb
+++ app/helpers/pulitzer/posts_helper.rb
@@ -1,19 +1,11 @@
 module Pulitzer
   module PostsHelper
 
-    def link_to_post_type(post_type)
+    def link_to_posts(post_type, plural_label='back to posts', singular_label='back to posts')
       if post_type.plural?
-        link_to "Index", posts_path(post_type_id: post_type.id)
+        link_to plural_label, posts_path(post_type_id: post_type.id)
       else
-        link_to "Manage", '#'
-      end
-    end
-
-    def link_back_to_post(post)
-      if post.post_type.plural?
-        link_to "Back to post", posts_path(post_type_id: post.post_type.id)
-      else
-        link_to "Back to post", '#'
+        link_to singular_label, post_path(post_type.singleton_post)
       end
     end
   end
