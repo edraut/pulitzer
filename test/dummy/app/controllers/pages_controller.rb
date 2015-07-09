0fb2d05c72dd18e17254c38b1b47bc40f276ac15
gomayonqui <escribimepues@gmail.com>
2015-07-09 15:18:01 -0500

    Add new_posts in the dummy app

--- test/dummy/app/controllers/pages_controller.rb
+++ test/dummy/app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def about_us
     @pulitzer_post = Pulitzer::PostType.named('About us').singleton_post
   end
+
+  def news_posts
+    @pulitzer_posts = Pulitzer::PostType.named('News articles').posts
+  end
 end
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   mount Pulitzer::Engine => "/pulitzer"
   get '/about-us', to: 'pages#about_us'
+  get '/news-posts', to: 'pages#news_posts'
 end
