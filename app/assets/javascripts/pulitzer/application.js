80e3bd401680ffb42f19eecb508ba0f2d4381dd4
gomayonqui <escribimepues@gmail.com>
2015-07-14 17:29:04 -0500

    Show info dynamically for text/images

--- app/assets/javascripts/pulitzer/application.js
+++ app/assets/javascripts/pulitzer/application.js
@@ -11,3 +11,13 @@
 // about supported directives.
 //
 //= require_tree .
+$(document).ready(function(){
+  $(document).on('change', '#post_type_content_element_type_content_element_type_id', function(){
+    $dimensionsFields = $(this).parent().find($('.image_dimensions')).first();
+    if ($('option:selected', this).text() == "Image"){
+      $dimensionsFields.show();
+    } else {
+      $dimensionsFields.hide();
+    }
+  });
+});
