9e1e0dedea8fe8fce886af79b3f28aa6795621fd
gomayonqui <escribimepues@gmail.com>
2015-07-20 17:09:56 -0500

    Add dynamic layout for pulitzer

--- app/controllers/pulitzer/application_controller.rb
+++ app/controllers/pulitzer/application_controller.rb
@@ -1,3 +1,4 @@
 class Pulitzer::ApplicationController < Pulitzer.base_controller
   helper Pulitzer::Engine.helpers
+  layout Pulitzer.layout
 end
--- lib/pulitzer.rb
+++ lib/pulitzer.rb
@@ -4,12 +4,13 @@ require 'hooch'
 require 'carrierwave'
 
 module Pulitzer
-  mattr_accessor  :base_controller, :metadata_closure
+  mattr_accessor  :base_controller, :metadata_closure, :layout
 
   def self.config(options)
     base_controller_name = options[:base_controller_name]
     @@base_controller = base_controller_name.constantize
     @@metadata_closure = options[:metadata_authorization]
+    @@layout = options[:layout] || 'application'
   end
 
   def self.skip_metadata_auth?
