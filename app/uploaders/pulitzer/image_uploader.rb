9185185fcd48a79c4e8052eaecf3f4086f49ee46
gomayonqui <escribimepues@gmail.com>
2015-08-13 16:47:52 -0500

    Add thumb version for images

--- app/uploaders/pulitzer/image_uploader.rb
+++ app/uploaders/pulitzer/image_uploader.rb
@@ -5,4 +5,8 @@ class Pulitzer::ImageUploader < Pulitzer::BaseUploader
   def resize_image
     self.class.process resize_to_fit: [model.height, model.width]
   end
+
+  version :thumb do
+    process resize_to_fill: [200,200]
+  end
 end
