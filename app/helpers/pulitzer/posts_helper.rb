bd2a303cb309d97fdb1f6e76331e6de0e38b8ff8
gomayonqui <escribimepues@gmail.com>
2015-08-13 11:44:47 -0500

    Fix manage/back to post links

--- app/helpers/pulitzer/posts_helper.rb
+++ app/helpers/pulitzer/posts_helper.rb
@@ -1,11 +1,19 @@
 module Pulitzer
   module PostsHelper
 
-    def link_to_posts(post_type, plural_label='back to posts', singular_label='back to posts')
+    def link_to_posts(post_type, plural_label, singular_label)
       if post_type.plural?
         link_to plural_label, posts_path(post_type_id: post_type.id)
       else
-        link_to singular_label, post_path(post_type.singleton_post)
+        link_to singular_label, post_content_elements_edit_multiple_path(post_type.singleton_post)
+      end
+    end
+
+    def link_back_to_posts(post_type, plural_label='back to posts', singular_label='back to posts')
+      if post_type.plural?
+        link_to plural_label, posts_path(post_type_id: post_type.id)
+      else
+        link_to singular_label, post_types_path
       end
     end
 
