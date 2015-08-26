f08cd3f9cc2ad928797a3dd08c96fc65376c1b79
gomayonqui <escribimepues@gmail.com>
2015-08-26 11:52:44 -0500

    Fixes #8 rename post content elements from post_type page

---
+++ app/interactions/pulitzer/create_post_type_content_elements.rb
@@ -0,0 +1,18 @@
+class Pulitzer::CreatePostTypeContentElements
+  attr_accessor :post_type, :ptcet, :old_label
+
+  def initialize(ptcet)
+    self.post_type  = ptcet.post_type
+    self.ptcet      = ptcet
+  end
+
+  def call
+    post_type.posts.each do |post|
+      post.content_elements.create do |ce|
+        ce.label = ptcet.label
+        ce.content_element_type = ptcet.content_element_type
+        ce.post_type_content_element_type = ptcet
+      end
+    end
+  end
+end
---
+++ app/interactions/pulitzer/update_post_type_content_elements.rb
@@ -0,0 +1,17 @@
+class Pulitzer::UpdatePostTypeContentElements
+  attr_accessor :post_type, :ptcet, :old_label
+
+  def initialize(ptcet, old_label=nil)
+    self.post_type  = ptcet.post_type
+    self.ptcet      = ptcet
+    self.old_label  = old_label || ptcet.label
+  end
+
+  def call
+    post_type.posts.each do |post|
+      post.content_elements.where(label: old_label).each do |ce|
+        ce.update(label: ptcet.label, content_element_type: ptcet.content_element_type, post_type_content_element_type: ptcet)
+      end
+    end
+  end
+end
