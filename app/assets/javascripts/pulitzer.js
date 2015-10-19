7ea0d0b6a5f6f98e696076d312cab5cbb5e219fa
gomayonqui <escribimepues@gmail.com>
2015-10-19 18:55:20 -0500

    Add a class to toggle post type content elements

--- app/assets/javascripts/pulitzer.js
+++ app/assets/javascripts/pulitzer.js
@@ -4,17 +4,33 @@ var Select2Trigger = Class.extend({
   }
 });
 
+var ToggleContentElementAttr = Class.extend({
+  init: function($list){
+    var self = this;
+    self.form = $list.parents("form");
+    selected = $list.find(':selected').text();
+    self.toggleAttr(selected);
+    $(document).on('change', $list, function(){
+      self.toggleAttr(selected);
+    });
+  },
+  toggleAttr: function(selected){
+    var self = this;
+    var type = selected.toLowerCase();
+    self.form.find("[data-content-element-attr-" + type + "]").toggle();
+  }
+});
+
+var RichTextEditor = Class.extend({
+  init: function($textarea){
+    self.editor = new wysihtml5.Editor($textarea[0], {
+      toolbar: 'toolbar',
+      parserRules:  wysihtml5ParserRules
+    });
+  }
+})
+
 $(document).ready(function(){
-  $(document).on('change', '#post_type_content_element_type_content_element_type_id', function(){
-    $dimensionsFields = $(this).parent().find($('.image_dimensions')).first();
-    if ($('option:selected', this).text() == "Image"){
-      $dimensionsFields.show();
-    } else {
-      $dimensionsFields.hide();
-    }
-  });
-
-  window.any_time_manager.registerList([ 'select2-trigger' ]);
+  window.any_time_manager.registerList([ 'select2-trigger', 'rich-text-editor', 'toggle-content-element-attr' ]);
   window.any_time_manager.load();
-
 });
