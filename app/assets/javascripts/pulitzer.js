4ccf68a37897347f6c4ecf5c98f74f39e7b1a891
gomayonqui <escribimepues@gmail.com>
2015-10-28 08:14:39 -0500

    Use hooch FormFieldRevealer in content_element types

--- app/assets/javascripts/pulitzer.js
+++ app/assets/javascripts/pulitzer.js
@@ -4,23 +4,6 @@ var Select2Trigger = Class.extend({
   }
 });
 
-var ToggleContentElementAttr = Class.extend({
-  init: function($list){
-    var self = this;
-    self.$form = $list.parents("form");
-    self.toggleAttr();
-    $(document).on('change', $list, function(){
-      self.toggleAttr();
-    });
-  },
-  toggleAttr: function(){
-    var self = this;
-    var selected = self.$form.find('[data-toggle-content-element-attr] option:selected').text().toLowerCase();
-    self.$form.find("[data-content-element-attr]").hide();
-    self.$form.find("[data-content-element-attr-" + selected + "]").show();
-  }
-});
-
 var RichTextEditor = Class.extend({
   init: function($textarea){
     self.$form = $textarea.parents("form");
@@ -33,6 +16,6 @@ var RichTextEditor = Class.extend({
 })
 
 $(document).ready(function(){
-  window.any_time_manager.registerList([ 'select2-trigger', 'rich-text-editor', 'toggle-content-element-attr' ]);
+  window.any_time_manager.registerList([ 'select2-trigger', 'rich-text-editor' ]);
   window.any_time_manager.load();
 });
