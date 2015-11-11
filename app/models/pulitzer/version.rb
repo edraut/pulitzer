1c8ca9f5e739dff8671a911f51b8328da692f8db
gomayonqui <escribimepues@gmail.com>
2015-11-11 17:32:04 -0500

    Add content_element finder to version

--- app/models/pulitzer/version.rb
+++ app/models/pulitzer/version.rb
@@ -4,5 +4,9 @@ module Pulitzer
     has_many :content_elements, dependent: :destroy
     has_many :post_tags, dependent: :destroy
     belongs_to :post
+
+    def content_element(label)
+      self.content_elements.find_by(label: label)
+    end
   end
 end
