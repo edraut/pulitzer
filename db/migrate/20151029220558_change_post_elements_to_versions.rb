99c508fdc37969021429db6ab8aa1bc26ae83c3f
gomayonqui <escribimepues@gmail.com>
2015-11-11 16:21:18 -0500

    Fix post migration

--- db/migrate/20151029220558_change_post_elements_to_versions.rb
+++ db/migrate/20151029220558_change_post_elements_to_versions.rb
@@ -2,24 +2,14 @@ class ChangePostElementsToVersions < ActiveRecord::Migration
   def up
     rename_column :pulitzer_content_elements, :post_id, :version_id
     rename_column :pulitzer_post_tags, :post_id, :version_id
-    Pulitzer::ContentElements.reset_column_information
-    Pulitzer::PostTags.reset_column_information
+    Pulitzer::ContentElement.reset_column_information
+    Pulitzer::PostTag.reset_column_information
 
     # Clone post content elements and clone tags, and assign them to the new version
     Pulitzer::Post.find_each do |post|
       version = post.create_preview_version
-      content_elements = Pulitzer::ContentElements.where(version_id: post.id)
-      post.content_elements.each do |ce|
-        element = ce.dup
-        element.version_id = nil
-        version.content_elements << element
-      end
-      content_elements = Pulitzer::PostTags.where(version_id: post.id)
-      post.post_tags.each do |pt|
-        post_tag = pt.dup
-        post_tag.version_id = nil
-        version.post_tags << post_tag
-      end
+      Pulitzer::ContentElement.where(version_id: post.id).update_all(version_id: version.id)
+      Pulitzer::PostTag.where(version_id: post.id).update_all(version_id: version.id)
     end
   end
 
