f477e1f4a40a3e4d63e5f6f4154726142cd6de07
gomayonqui <escribimepues@gmail.com>
2015-11-10 23:48:33 -0500

    Add content elements preview link

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#news_posts'
   get '/about-us', to: 'pages#about_us'
   get '/news-posts', to: 'pages#news_posts'
+
+  namespace :pulitzer_preview do
+    get '/about-us', to: 'pages#about_us', as: 'about_us'
+  end
 end
