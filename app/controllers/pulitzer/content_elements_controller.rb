1ff924a44f4ef862cb9dab4a0dd77dc82a092bea
gomayonqui <escribimepues@gmail.com>
2015-11-13 18:11:25 -0500

    Add content element free form views

--- app/controllers/pulitzer/content_elements_controller.rb
+++ app/controllers/pulitzer/content_elements_controller.rb
@@ -1,11 +1,23 @@
 class Pulitzer::ContentElementsController < Pulitzer::ApplicationController
-  before_filter :get_content_element, only: [:show, :edit, :update]
+  before_filter :set_content_element, only: [:show, :edit, :update]
 
   def index
     @post             = Pulitzer::Post.find(params[:post_id])
     @content_elements = @post.preview_version.content_elements
     route             = "pulitzer_preview_#{@post.title.parameterize('_')}_path"
     @preview_path     = main_app.public_send(route) if main_app.respond_to?(route)
+  end
+
+  def new
+    @version          = Pulitzer::Version.find params[:version_id]
+    @content_element  = @version.content_elements.build
+    render partial: 'new', locals: { content_element: @content_element, version: @version }
+  end
+
+  def create
+    @version         = Pulitzer::Version.find content_element_params[:version_id]
+    @content_element = @version.content_elements.create content_element_params
+    render partial: 'show_wrapper', locals: { content_element: @content_element }
   end
 
   def show
@@ -27,7 +39,7 @@ class Pulitzer::ContentElementsController < Pulitzer::ApplicationController
     params[:content_element].permit!
   end
 
-  def get_content_element
+  def set_content_element
     @content_element = Pulitzer::ContentElement.find(params[:id])
   end
 end
--- app/models/pulitzer/content_element.rb
+++ app/models/pulitzer/content_element.rb
@@ -5,7 +5,6 @@ module Pulitzer
     belongs_to :content_element_type
     belongs_to :post_type_content_element_type
     delegate :type, :text_type?, :image_type?, :video_type?, to: :content_element_type
-    delegate :height, :width, :text_editor, to: :post_type_content_element_type
     delegate :post, to: :version
     default_scope { order(id: :asc) }
 
