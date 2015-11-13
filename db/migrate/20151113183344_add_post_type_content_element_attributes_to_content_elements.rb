09aa5a16cf40a8ee994ece9d81c094749d77e77d
gomayonqui <escribimepues@gmail.com>
2015-11-13 18:11:09 -0500

    Add post type content element attributes to content elements

---
+++ db/migrate/20151113183344_add_post_type_content_element_attributes_to_content_elements.rb
@@ -0,0 +1,10 @@
+class AddPostTypeContentElementAttributesToContentElements < ActiveRecord::Migration
+  def change
+    add_column :pulitzer_content_elements, :text_editor, :string
+    add_column :pulitzer_content_elements, :height, :integer, default: 100
+    add_column :pulitzer_content_elements, :width, :integer, default: 100
+    add_column :pulitzer_content_elements, :sort_order, :integer
+    Pulitzer::ContentElement.reset_column_information
+    Pulitzer::PostTypeContentElementType
+  end
+end
