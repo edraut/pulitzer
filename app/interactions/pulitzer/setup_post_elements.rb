c7069fd0c8b1ceaf1773a3f760836aefaa0286e6
gomayonqui <escribimepues@gmail.com>
2015-11-16 11:00:36 -0500

    Update interactions to copy new content elements attributes

--- app/interactions/pulitzer/setup_post_elements.rb
+++ app/interactions/pulitzer/setup_post_elements.rb
@@ -8,8 +8,11 @@ class Pulitzer::SetupPostElements
   def call
     post.post_type_content_element_types.each do |cet|
       post.preview_version.content_elements.create do |ce|
-        ce.label = cet.label
-        ce.content_element_type = cet.content_element_type
+        ce.label                          = cet.label
+        ce.height                         = cet.height
+        ce.width                          = cet.width
+        ce.text_editor                    = cet.text_editor
+        ce.content_element_type           = cet.content_element_type
         ce.post_type_content_element_type = cet
       end
     end
