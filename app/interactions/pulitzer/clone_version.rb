d58ff328a6d11262b1e30fc3c2530cf35d0899b9
gomayonqui <escribimepues@gmail.com>
2015-12-07 00:37:03 -0500

    Rescue no-existing image

--- app/interactions/pulitzer/clone_version.rb
+++ app/interactions/pulitzer/clone_version.rb
@@ -9,7 +9,7 @@ class Pulitzer::CloneVersion
   def call
     version.content_elements.each do |ce|
       element = ce.dup
-      element.image = ce.image.file if ce.image_url
+      clone_image(ce, element)
       element.version_id = nil
       new_version.content_elements << element
     end
@@ -17,6 +17,17 @@ class Pulitzer::CloneVersion
       post_tag = pt.dup
       post_tag.version_id = nil
       new_version.post_tags << post_tag
+    end
+  end
+
+private
+  def clone_image(original, destination)
+    if original.image.file && original.image.file.exists?
+      begin
+        destination.image = original.image.file
+      rescue
+        destination.remove_image!
+      end
     end
   end
 end
