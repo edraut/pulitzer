5e77f1667dc4a30f674872d2fa0706b9f6f6f77c
Leonardo Bighetti <leonardo.bighetti@gmail.com>
2016-05-17 18:54:20 -0300

    add named, active_posts to tags

--- app/models/pulitzer/tag.rb
+++ app/models/pulitzer/tag.rb
@@ -1,6 +1,7 @@
 module Pulitzer
   class Tag < ActiveRecord::Base
     has_many :post_tags, as: :label, dependent: :destroy
+    has_many :posts,   through: :post_tags
     has_many :versions, through: :post_tags
     has_many :children, class_name: 'Pulitzer::Tag', dependent: :destroy, foreign_key: :parent_id
     belongs_to :parent, class_name: 'Pulitzer::Tag'
@@ -8,8 +9,13 @@ module Pulitzer
     validates :name, presence: true, uniqueness: true
 
     scope :hierarchical, -> { where hierarchical: true  }
-    scope :flat, -> { where hierarchical: false }
-    scope :root, -> { hierarchical.where(parent_id: nil) }
+    scope :flat,  -> { where hierarchical: false }
+    scope :root,  -> { hierarchical.where(parent_id: nil) }
+    scope :named, -> { |name| where(name: name) }
+
+    def active_posts
+      @active_posts ||= posts.map(&:active_version).compact
+    end
 
     def root?
       hierarchical? && parent_id.nil?
