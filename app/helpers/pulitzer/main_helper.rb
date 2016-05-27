292991b9498bbaa2eff0cdbed531eb6a644fc5ef
Leonardo Bighetti <leonardo.bighetti@gmail.com>
2016-05-27 16:59:18 -0300

    changed helper method name to avoid possible name clashing

--- app/helpers/pulitzer/main_helper.rb
+++ app/helpers/pulitzer/main_helper.rb
@@ -1,7 +1,7 @@
 module Pulitzer
   module MainHelper
     # Will this clash with helper methods like devise, cancan?
-    def authorized?
+    def pulitzer_authorized?
       Pulitzer.skip_metadata_auth? || self.instance_eval(&Pulitzer.metadata_closure)
     end
   end
