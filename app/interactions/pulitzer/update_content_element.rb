386823a9c92319e027c9e41fbe24aa74ba2d3819
Eric Draut <edraut@gmail.com>
2016-07-05 11:42:18 -0600

    Kramdown not ready for use, minor fix though

--- app/interactions/pulitzer/update_content_element.rb
+++ app/interactions/pulitzer/update_content_element.rb
@@ -7,6 +7,7 @@ class Pulitzer::UpdateContentElement
   def call
     text_editor = Pulitzer.text_editor_toolbars.detect { |toolbar| toolbar[:name] == content_element.text_editor }
     if 'Kramdown' == text_editor[:kind]
+      params[:markdown] = params[:body]
       params[:body] = Kramdown::Document.new(params[:body]).to_html
     end
     @content_element.update params
