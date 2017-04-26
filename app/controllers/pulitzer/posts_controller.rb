7c28d0cd549aa54f9a036af90bec94407830ac00
Jean Pierre Guarin <escribimepues@gmail.com>
2017-04-26 16:56:13 -0500

    Reload version page when updating slug

--- app/controllers/pulitzer/posts_controller.rb
+++ app/controllers/pulitzer/posts_controller.rb
@@ -1,6 +1,7 @@
 class Pulitzer::PostsController < Pulitzer::ApplicationController
-  before_filter :get_post, only: [:show, :edit, :edit_title, :edit_slug, :update,
+  before_filter :get_post, only: [:show, :edit, :edit_title, :edit_slug, :show_slug, :update,
     :update_slug, :processing_preview]
+  before_action :get_version, only: [:edit_slug, :show_slug, :update_slug]
 
   def index
     @post_type = Pulitzer::PostType.find params[:post_type_id]
@@ -44,23 +45,31 @@ class Pulitzer::PostsController < Pulitzer::ApplicationController
 
   def edit_slug
     if request.xhr?
-      render partial: 'form_slug', locals: { post: @post }
+      render partial: 'form_slug', locals: { post: @post, version: @version }
     end
   end
 
   def show_slug
-    render partial: 'show_slug', locals: { post: @post }
+    render partial: 'show_slug', locals: { post: @post, version: @version }
   end
 
   def update_slug
     @post.update_attributes(post_params)
-    render partial: 'show_slug', locals: { post: @post }
+    if @version.preview?
+      route                       = "#{Pulitzer.preview_namespace}_#{@post.post_type.name.parameterize('_')}_path"
+      @preview_path               = main_app.public_send(route, @post.slug) if main_app.respond_to?(route)
+    end
+    render partial: 'pulitzer/posts/edit', locals: { version: @version, post: @post }
   end
 
   protected
 
   def post_params
     params[:post].permit!
+  end
+
+  def get_version
+    @version = Pulitzer::Version.find(params[:version_id])
   end
 
   def get_post
