f186c0bb26f778564f83352a17c9ba724f497617
Eric Draut <edraut@gmail.com>
2017-05-22 13:16:52 -0600

    stupid sqlite doesn't know how to handle json

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -149,7 +149,7 @@ ActiveRecord::Schema.define(version: 20170518145318) do
     t.integer  "post_id"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
-    t.         "cloning_errors"
+    t.text     "cloning_errors"
   end
 
 end
