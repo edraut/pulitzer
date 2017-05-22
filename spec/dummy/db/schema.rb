469e7546e10a25430d161b5f1b10ff89d988bdd8
Eric Draut <edraut@gmail.com>
2017-05-22 10:51:21 -0600

    sqlite can't handle json types

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -113,7 +113,7 @@ ActiveRecord::Schema.define(version: 20170502210827) do
     t.integer  "post_id"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
-    t.blob     "cloning_errors"
+    t.text     "cloning_errors"
   end
 
 end
