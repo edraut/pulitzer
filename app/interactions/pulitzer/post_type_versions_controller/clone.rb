f484ccc44d11c530489515f86aabe1e87fb3e679
Jean Pierre Guarin <escribimepues@gmail.com>
2017-06-21 10:50:34 -0500

    add Clone partials

--- app/interactions/pulitzer/post_type_versions_controller/clone.rb
+++ app/interactions/pulitzer/post_type_versions_controller/clone.rb
@@ -18,15 +18,55 @@ class Pulitzer::PostTypeVersionsController::Clone
       new_version.processed_element_count += 1
       new_version.broadcast_change if defined? ForeignOffice
     end
-    published_type_version.free_form_section_types.each do |ffst|
-      begin
-        new_version.free_form_section_types << ffst.clone_me
-      rescue ActiveRecord::RecordInvalid => invalid
-        cloning_errors.push "Free Form Section Type #{ffst.id} could not be cloned: #{invalid.record.errors.full_messages.join(', ')}"
+    if new_version.kind == 'partial'
+      published_type_version.background_styles.each do |bs|
+        begin
+          new_version.background_styles << bs.clone_me
+        rescue ActiveRecord::RecordInvalid => invalid
+          cloning_errors.push "Background Styles #{bs.id} could not be cloned: #{invalid.record.errors.full_messages.join(', ')}"
+        end
+        new_version.processed_element_count += 1
+        new_version.broadcast_change if defined? ForeignOffice
       end
-      new_version.processed_element_count += 1
-      new_version.broadcast_change if defined? ForeignOffice
+      published_type_version.justification_styles.each do |js|
+        begin
+          new_version.justification_styles << js.clone_me
+        rescue ActiveRecord::RecordInvalid => invalid
+          cloning_errors.push "Justification Styles #{js.id} could not be cloned: #{invalid.record.errors.full_messages.join(', ')}"
+        end
+        new_version.processed_element_count += 1
+        new_version.broadcast_change if defined? ForeignOffice
+      end
+      published_type_version.sequence_flow_styles.each do |sfs|
+        begin
+          new_version.sequence_flow_styles << sfs.clone_me
+        rescue ActiveRecord::RecordInvalid => invalid
+          cloning_errors.push "Sequence Flow Styles #{sfs.id} could not be cloned: #{invalid.record.errors.full_messages.join(', ')}"
+        end
+        new_version.processed_element_count += 1
+        new_version.broadcast_change if defined? ForeignOffice
+      end
+      published_type_version.arrangement_styles.each do |as|
+        begin
+          new_version.arrangement_styles << as.clone_me
+        rescue ActiveRecord::RecordInvalid => invalid
+          cloning_errors.push "Arrangement Styles #{as.id} could not be cloned: #{invalid.record.errors.full_messages.join(', ')}"
+        end
+        new_version.processed_element_count += 1
+        new_version.broadcast_change if defined? ForeignOffice
+      end
+    elsif new_version.kind == 'template'
+      published_type_version.free_form_section_types.each do |ffst|
+        begin
+          new_version.free_form_section_types << ffst.clone_me
+        rescue ActiveRecord::RecordInvalid => invalid
+          cloning_errors.push "Free Form Section Type #{ffst.id} could not be cloned: #{invalid.record.errors.full_messages.join(', ')}"
+        end
+        new_version.processed_element_count += 1
+        new_version.broadcast_change if defined? ForeignOffice
+      end
     end
+    #Pulitzer::CloneVersionJob.perform_later(published_type_version, new_version)
     new_version
   end
 
--- app/models/pulitzer/arrangement_style.rb
+++ app/models/pulitzer/arrangement_style.rb
@@ -4,5 +4,12 @@ module Pulitzer
     has_many :partials
     validates :display_name, presence: true
     validates :view_file_name, presence: true
+
+    def clone_me
+      clone_attrs = self.attributes.dup
+      clone_attrs.delete 'id'
+      clone_attrs.delete 'post_type_version_id'
+      Pulitzer::ArrangementStyle.new(clone_attrs)
+    end
   end
 end
